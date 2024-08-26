151c7b1b9323484336379c38f2157ad1ca7469e0
Rebeca Ouriques <ouriques.rebecah@gmail.com>
2024-08-26 19:24:00 -0300

    Configuração de carrinho

--- src/Components/Produto/CardCarrinho/CardCarrinho.tsx
+++ src/Components/Produto/CardCarrinho/CardCarrinho.tsx
@@ -6,55 +6,116 @@ interface CardProdutosProps {
   item: Produto;
 }
 
-function CardCarrinho({ item }: CardProdutosProps) {
-  const { adicionarProduto, adicionarQuantidadeProduto, removerQuantidadeProduto, removerProduto } = useContext(AuthContext);
-
+function MinusIcon(props) {
   return (
-    <div className='shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)] rounded-md'>
-      <div className="relative h-40">
-        <img src={item.imagem} className="h-full w-full object-cover" alt="" />
-      </div>
-      <div className='p-4'>
-        <div className="mb-2 flex items-center justify-between text-md md:text-xl">
-          <p className="text-hoodaLaranja font-medium">{item.nome}</p>
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M5 12h14" />
+    </svg>
+  )
+}
+
+
+function PlusIcon(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M5 12h14" />
+      <path d="M12 5v14" />
+    </svg>
+  )
+}
+
+
+function TrashIcon(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M3 6h18" />
+      <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
+      <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" />
+    </svg>
+  )
+}
+
+function CardCarrinho({ item }: CardProdutosProps) {
+  const { adicionarQuantidadeProduto, removerQuantidadeProduto, removerProduto } = useContext(AuthContext);
+
+  const total = item.quantidade * item.preco
+  return (
+    <div className='bg-white shadow-md rounded-lg my-4 p-1 gap-4'>
+     
+        <div className="relative h-32 w-full md:h-48 md:w-full md:col-span-1 ">
+          <img src={item.imagem} className="h-full w-full object-cover rounded-md" alt={item.nome} />
         </div>
-        <div className="mb-2 flex items-center justify-between text-md">
-          <p className="text-hoodaLaranja font-medium">R$ {item.preco.toFixed(2)}</p>
+
+        <div className="ml-4 md:col-span-1">
+          <p className="text-base text-center p-2 text-amber-400 font-semibold md:text-2xl lg:text-2xl md:flex md:justify-center md:items-center">{item.nome}</p>
         </div>
-        <p className="text-gray-600 text-sm font-normal opacity-75">
-          Descrição: {item.descricao}
-        </p>
 
-        <p className="text-gray-600 text-sm font-normal opacity-75">
-          Avaliação: {item.avaliacao}
-        </p>
-
-        <p className="text-gray-600 text-sm font-normal opacity-75">
-          Quantidade:
-          <button className=" ml-2 bg-red-400 py-0 px-2 rounded-full"
-            onClick={() => removerQuantidadeProduto(item.id)} disabled={item.quantidade <= 1} /* Desativa o botão se a quantidade for 1 */ >
-            -
-          </button> {item.quantidade}  <button className="mr-2 bg-amber-400 py-0 px-2 rounded-full"
-            onClick={() => adicionarQuantidadeProduto(item.id)}> + </button>
-        </p>
-
-        <p className="text-gray-600 text-sm font-normal opacity-75">
-          Subtotal: R$ {item.quantidade * item.preco}
-        </p>
+      <div className='flex justify-between p-1 md:col-span-1'>
+      <div className="flex items-center justify-start md:justify-center">
+        <p className="text-lg font-semibold text-gray-900 md:text-2xl">R$ {item.preco.toFixed(2)}</p>
       </div>
-
-      <div className="p-4 border-t border-gray-200 flex justify-evenly">
-
-
-
-
+      <div className="flex items-center justify-center space-x-2">
         <button
-          className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full"
-          onClick={() => removerProduto(item.id)}
+          className="bg-red-500 hover:bg-red-700 text-white rounded-full"
+          onClick={() => removerQuantidadeProduto(item.id)}
+          disabled={item.quantidade <= 1}
         >
-          Remover Produto
+          <MinusIcon className="h-4 w-4 md:h-6 md:w-6" />
+        </button>
+        <span className="text-lg font-medium md:text-2xl">{item.quantidade}</span>
+        <button
+          className="bg-amber-400 hover:bg-amber-600 text-white rounded-full"
+          onClick={() => adicionarQuantidadeProduto(item.id)}
+        >
+          <PlusIcon className="h-4 w-4 md:h-6 md:w-6" />
         </button>
       </div>
+      <div className="flex items-center justify-center col-span-1">
+        <p className="text-lg font-medium text-gray-900 md:text-2xl">R$ {total.toFixed(2)}</p>
+      </div>
+      </div>
+      <div className="col-span-1 flex items-center justify-center">
+        <button
+          className="w-full bg-red-500 hover:bg-red-700 my-2 text-white font-bold rounded-full md:text-2xl"
+          onClick={() => removerProduto(item.id)}
+        >
+          Remover
+        </button>
+      </div>
+
     </div>
   );
 }
--- src/Pages/cart/Cart.tsx
+++ src/Pages/cart/Cart.tsx
@@ -7,37 +7,58 @@ function Cart() {
 
     const calcularSubtotal = () => {
         return items.reduce((total, produto) => total + produto.preco * produto.quantidade, 0);
-      };
-    
-      // Calcula o subtotal total
-      const subtotal = calcularSubtotal();
+    };
 
-console.log(items)
+    // Calcula o subtotal total
+    const subtotal = calcularSubtotal();
+    
+
+    console.log(items)
     return (
         <>
 
-            <div className='flex flex-col'>
-                <div className="flex justify-center w-full my-4">
-                    <div className="container flex flex-col">
-                        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 mx-2 md:mx-3">
+            <div className='flex flex-col md:mx-6'>
+                <div className='font-Montserrat text-hoodaLaranja text-xl pt-2 flex justify-center md:justify-start md:ml-1'>Carrinho de Compras</div>
+                <div className="container grid grid-cols-1 justify-between w-full my-4 md:grid-cols-6 md:flex ">
+
+                    <div className="container col-span-3 bg-white shadow-md rounded-lg p-6">
+                        <div className='grid grid-cols-5 items-center font-Montserrat'>
+                            {/* <div className='col-span-1 flex justify-center text-sm'>Produto</div>
+                            <div className='col-span-1 flex justify-center text-sm'>Preço</div>
+                            <div className='col-span-1 flex justify-center'>Quantidade</div>
+                            <div className='col-span-1 flex justify-center'>Subtotal</div> */}
+                        </div>
+                        <div className="">
                             {
                                 items.map(produto => (
-                                    <CardProdutos1 key={produto.id} item={produto}/>
+                                    <CardProdutos1 key={produto.id} item={produto} />
                                 ))
                             }
                         </div>
                     </div>
+                    <div className='container col-span-2 bg-white shadow-md rounded-lg p-6 mt-4 text-center font-Montserrat text-xl md:mt-0 md:ml-6 md:text-2xl md:h-[50vh] md:pb-4 lg:h[50vh]'>
+                    <div className=' pb-2 md:pb-4'>Resumo do Pedido</div>
+                    <div className='flex justify-between  pt-2 font-Montserrat text-sm px-2 md:text-xl md:py-2 '>
+                        <span>Subtotal: </span> <span> R$ {subtotal.toFixed(2)}</span>
+                        
+                    </div>
+                    <div className='flex justify-between pb-2 font-Montserrat text-sm px-2 md:text-xl md:pb-4'>
+                        <span>Frete: </span> <span>R$ 0,00</span>
+                    </div>
+                    <div  className='flex justify-between pt-4 px-2 font-Montserrat border-t-2 border-slate-400 text-lg md:text-xl md:pt-4'>
+                    <span>Total: </span> <span> R$ {subtotal.toFixed(2)}</span> 
+                    </div>
+                    <div className='flex justify-center m-2 pt-6'>
+                        <button className="bg-hoodaLaranja hover:bg-amber-400 text-white font-bold py-2 px-4 rounded-full"
+                            onClick={limparCart}>Finalizar Compra</button>
+                    </div>
+                    </div>
                 </div>
             </ div >
-            <div className='flex justify-end bg-amber-400 p-4 font-Montserrat'>
-          <span>Total: R$ {subtotal.toFixed(2)}</span>
-        </div>
-            <div className='flex justify-center m-2 pt-6'> 
-                <button className="bg-hoodaLaranja hover:bg-amber-400 text-white font-bold py-2 px-4 rounded-full"
-                    onClick={limparCart}>Finalizar Compra</button>
-            </div>
 
-            
+
+
+
         </>
     )
 }
