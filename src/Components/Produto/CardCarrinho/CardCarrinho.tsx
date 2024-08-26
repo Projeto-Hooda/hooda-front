d0915684b8f94e08cced19ccde09fdc3c8e4dba0
Rebeca Ouriques <ouriques.rebecah@gmail.com>
2024-08-26 12:29:01 -0300

    Adição botão de quantidade no carrinho

--- src/Components/Produto/CardCarrinho/CardCarrinho.tsx
+++ src/Components/Produto/CardCarrinho/CardCarrinho.tsx
@@ -1,55 +1,62 @@
-import { useContext } from 'react'
-import { AuthContext } from '../../../contexts/AuthContext'
-
-import  Produto  from "../../../model/Produto"
+import { useContext } from 'react';
+import { AuthContext } from '../../../contexts/AuthContext';
+import Produto from "../../../model/Produto";
 
 interface CardProdutosProps {
-    item: Produto
+  item: Produto;
 }
 
 function CardCarrinho({ item }: CardProdutosProps) {
-    const { adicionarProduto, removerProduto } = useContext(AuthContext)
+  const { adicionarProduto, adicionarQuantidadeProduto, removerQuantidadeProduto, removerProduto } = useContext(AuthContext);
 
-    return (
-        <div className='shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)] rounded-md'>
-        <div className="relative h-40">
-          <img src={item.imagem} className="h-full w-full object-cover" alt="" />
-
+  return (
+    <div className='shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)] rounded-md'>
+      <div className="relative h-40">
+        <img src={item.imagem} className="h-full w-full object-cover" alt="" />
+      </div>
+      <div className='p-4'>
+        <div className="mb-2 flex items-center justify-between text-md md:text-xl">
+          <p className="text-hoodaLaranja font-medium">{item.nome}</p>
         </div>
-        <div className='p-4'>
-          <div className="mb-2 flex items-center justify-between text-md md:text-xl">
-            <p className="text-hoodaLaranja font-medium">{item.nome}</p>
-          </div>
-          <div className="mb-2 flex items-center justify-between text-md">
-            <p className="text-hoodaLaranja font-medium">R$ {item.preco.toFixed(2)}</p>
-          </div>
-          <p className="text-gray-600 text-sm font-normal opacity-75">
-            Descrição: {item.descricao}
-          </p>
-
-          <p className="text-gray-600 text-sm font-normal opacity-75">
-            Avaliação: {item.avaliacao}
-          </p>
-
-          <p className="text-gray-600 text-sm font-normal opacity-75">
-            Quantidade: {item.quantidade}
-          </p>
-
-          <p className="text-gray-600 text-sm font-normal opacity-75">
-            Subtotal: {item.quantidade * item.preco}
-          </p>
-
+        <div className="mb-2 flex items-center justify-between text-md">
+          <p className="text-hoodaLaranja font-medium">R$ {item.preco.toFixed(2)}</p>
         </div>
+        <p className="text-gray-600 text-sm font-normal opacity-75">
+          Descrição: {item.descricao}
+        </p>
 
-        <div className="p-4 border-t border-gray-200 flex justify-evenly">
-        <button className="bg-hoodaLaranja hover:bg-amber-400 text-white font-bold py-2 px-4 rounded-full"
-                    onClick={() => adicionarProduto(item)}>Adicionar</button>
+        <p className="text-gray-600 text-sm font-normal opacity-75">
+          Avaliação: {item.avaliacao}
+        </p>
 
-                <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full"
-                    onClick={() => removerProduto(item.id)}>Remover</button>
-            </div>
-        </div>
-    )
+        <p className="text-gray-600 text-sm font-normal opacity-75">
+          Quantidade:
+          <button className=" ml-2 bg-red-400 py-0 px-2 rounded-full"
+            onClick={() => removerQuantidadeProduto(item.id)} disabled={item.quantidade <= 1} /* Desativa o botão se a quantidade for 1 */ >
+            -
+          </button> {item.quantidade}  <button className="mr-2 bg-amber-400 py-0 px-2 rounded-full"
+            onClick={() => adicionarQuantidadeProduto(item.id)}> + </button>
+        </p>
+
+        <p className="text-gray-600 text-sm font-normal opacity-75">
+          Subtotal: R$ {item.quantidade * item.preco}
+        </p>
+      </div>
+
+      <div className="p-4 border-t border-gray-200 flex justify-evenly">
+
+
+
+
+        <button
+          className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full"
+          onClick={() => removerProduto(item.id)}
+        >
+          Remover Produto
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default CardCarrinho
+export default CardCarrinho;
