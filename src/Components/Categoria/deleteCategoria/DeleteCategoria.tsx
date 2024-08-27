6600732e56f5948a51f4df748f60075c44106999
Rebeca Ouriques <ouriques.rebecah@gmail.com>
2024-08-27 19:57:55 -0300

    ajuste dos cards de deleção

--- src/Components/Categoria/deleteCategoria/DeleteCategoria.tsx
+++ src/Components/Categoria/deleteCategoria/DeleteCategoria.tsx
@@ -26,7 +26,7 @@ function DeleteCategoria() {
             })
         } catch (error: any) {
             if (error.toString().includes('403')) {
-                toastAlerta('O token expirou, favor logar novamente','info')
+                toastAlerta('O token expirou, favor logar novamente', 'info')
                 handleLogout()
             }
         }
@@ -34,7 +34,7 @@ function DeleteCategoria() {
 
     useEffect(() => {
         if (token === '') {
-            toastAlerta('Você precisa estar logado','info')
+            toastAlerta('Você precisa estar logado', 'info')
             navigate('/login')
         }
     }, [token])
@@ -67,28 +67,31 @@ function DeleteCategoria() {
     }
     return (
 
-        <div className='container flex justify-center mt-20'>
-        <div className="relative flex flex-col justify-center text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-96">
-  <div className="p-6 ">
-  <h3 className='text-lg font-bold text-center uppercase m-7'>Deletar categoria {categoria.nome}?</h3>
-  <p className='flex justify-center'>{categoria.descricao}</p>
-  </div>
-  <div className="p-6 pt-0 flex justify-evenly">
-  
-    <button
-      className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-hoodaLaranja text-white shadow-md hover:shadow-lg hover:bg-amber-400 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none m-4 "
-      type="button" onClick={retornar}>
-     Não
-    </button>
-     
-    <button
-      className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-red-600 text-white shadow-md hover:shadow-lg hover:bg-amber-400 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none m-4"
-      type="button" onClick={deletarCategorias}>
-     Sim
-    </button>
+        <div className='flex justify-center mt-20 mx-2'>
+            <div className="relative flex flex-col justify-center text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-96">
+                <div className="p-6 ">
+                    <h3 className='text-lg font-bold text-center uppercase my-3 font-sans'>Deletar categoria:</h3>
+                    <p className='text-xl flex justify-center my-3 font-semibold font-sans'>{categoria.nome}</p>
+                    <p className='flex justify-center my-2 font-sans'>({categoria.descricao})</p>
+                </div>
+                <p className='flex justify-center m-1 font-semibold font-sans'>Tem certeza que você quer deletar {categoria.nome}?</p>
+                <div className="p-6 pt-0 flex justify-evenly">                
+                <button
+                        className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-hoodaLaranja text-white shadow-md hover:shadow-lg hover:bg-amber-400 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none m-4"
+                        type="button" onClick={deletarCategorias}>
+                        Sim
+                    </button>
+
+                    <button
+                        className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-red-600 text-white shadow-md hover:shadow-lg hover:bg-red-400 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none m-4 "
+                        type="button" onClick={retornar}>
+                        Não
+                    </button>
+
+
+                </div>
+            </div>
         </div>
-    </div>
-</div>
     )
 }
 
